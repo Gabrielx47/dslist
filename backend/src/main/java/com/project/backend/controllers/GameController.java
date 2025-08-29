0204c96ed883501a3568e93b978853950f265d27
Gabrielx47 <62863349+Gabrielx47@users.noreply.github.com>
2025-08-29 00:16:31 -0300

    feat: get game list data

---
+++ backend/src/main/java/com/project/backend/controllers/GameController.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.project.backend.controllers;
+
+import com.project.backend.dto.GameMinDTO;
+import com.project.backend.entities.Game;
+import com.project.backend.services.GameService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/games")
+public class GameController {
+    @Autowired
+    private GameService gameService;
+    
+    @GetMapping
+    public List<GameMinDTO> findAll() {
+        List<GameMinDTO> result = gameService.findAll();
+        return result;
+    }
+}
