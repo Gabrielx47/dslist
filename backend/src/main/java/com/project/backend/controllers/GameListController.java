094d76763571c2c851e1ca6ff564c9fc5426b95d
Gabriel Mendonça <galeraxxx76@gmail.com>
2025-09-21 00:23:55 -0300

    feat: implement endpoint to get list of games sorted by category and ordered by position

--- backend/src/main/java/com/project/backend/controllers/GameListController.java
+++ backend/src/main/java/com/project/backend/controllers/GameListController.java
@@ -2,10 +2,13 @@ package com.project.backend.controllers;
 
 import com.project.backend.dto.GameDTO;
 import com.project.backend.dto.GameListDTO;
+import com.project.backend.dto.GameMinDTO;
 import com.project.backend.services.GameListService;
+import com.project.backend.services.GameService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,10 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class GameListController {
     @Autowired
     private GameListService gameListService;
+    @Autowired
+    private GameService gameService;
     
     @GetMapping
     public List<GameListDTO> findAll() {
         List<GameListDTO> result = gameListService.findAll();
+        return result;
+    }
+    
+    @GetMapping(value = "/{listId}/games")
+    public List<GameMinDTO> findByList(@PathVariable Long listId) {
+        List<GameMinDTO> result = gameService.findByList(listId);
         return result;
     }
 }
--- backend/src/main/java/com/project/backend/services/GameService.java
+++ backend/src/main/java/com/project/backend/services/GameService.java
@@ -3,6 +3,7 @@ package com.project.backend.services;
 import com.project.backend.dto.GameDTO;
 import com.project.backend.dto.GameMinDTO;
 import com.project.backend.entities.Game;
+import com.project.backend.projections.GameMinProjection;
 import com.project.backend.repositories.GameRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,6 +24,12 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
+        return result.stream().map(x -> new GameMinDTO(x)).toList();
+    }
+    
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long id) {
+        List<GameMinProjection> result = gameRepository.searchByList(id);
         return result.stream().map(x -> new GameMinDTO(x)).toList();
     }
 }
